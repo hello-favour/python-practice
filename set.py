31d8f698f752fd42ee2e47c58ac5b8d210aa8441
Favour <hello.williamsfav@gmail.com>
2023-08-07 03:52:00 -0700

    i learn about set and list

---
+++ set.py
@@ -0,0 +1,12 @@
+my_set = {"january","febuary","match","april"}
+for element in my_set:
+    print(element)
+my_set.add("july")
+print(my_set)
+
+my_set.remove("match")
+print(my_set)
+
+my_list = ["september","october","november","december"]
+my_list.remove("november")
+print(my_list)

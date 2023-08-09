8bef13a9c242b05909538b31e92a20ad537d4fa8
Favour <hello.williamsfav@gmail.com>
2023-08-09 13:43:19 -0700

    learn class function

---
+++ another_post.py
@@ -0,0 +1,7 @@
+class Anotherpost:
+    def __init__(self, message,author):
+        self.message = message,
+        self.author = author,
+
+    def get_another_post_info(self):
+        print(f"post {self.message} written by {self.author}")
--- post.py
+++ post.py
@@ -1,4 +1,5 @@
 from class_function import User
+from another_post import  Anotherpost
 
 app_user_info = User("favour@gmail.com","favour", "123", "developer")
 app_user_info.get_user_info()
@@ -8,3 +9,6 @@ app_user_info.get_user_info()
 
 app_user_info.change_password("flutter")
 app_user_info.get_user_info()
+
+app_another_user_info = Anotherpost("on a secret mission today", "favor")
+app_another_user_info.get_another_post_info()
